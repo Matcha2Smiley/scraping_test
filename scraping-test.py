aa0faab2e34f49c8a6f497e29486834f20f1aff6
Matcha2Smiley <greentea2Smiley@gmail.com>
2024-02-01 18:48:39 +0900

    QiitaでJavaを調べてみた

--- scraping-test.py
+++ scraping-test.py
@@ -1,28 +1,32 @@
 import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-
+from selenium.webdriver.common.keys import Keys
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+chrome_options = Options()
+chrome_options.add_experimental_option("detach", True)
 
-driver = webdriver.Chrome()
+driver = webdriver.Chrome(options=chrome_options)
 
-driver.get("https://researchers.general.hokudai.ac.jp/search/index.html")
+driver.get("https://qiita.com/")
 
 title = driver.title
 
 driver.implicitly_wait(0.5)
 
-text_box = driver.find_element(by=By.NAME, value="search-keyword")
-submit_button = driver.find_element(by=By.NAME, value="mFreeword")
+text_box = driver.find_element(by=By.CLASS_NAME, value="style-7sxcw7")
+# submit_button = driver.find_element(by=By.NAME, value="mFreeword")
 
-text_box.send_keys("光合成")
-submit_button.click()
+text_box.send_keys("Java")
+text_box.send_keys(Keys.ENTER)
+# submit_button.click()
 
 message = driver.find_element(by=By.ID, value="message")
 text = message.text
 
 time.sleep(50)
 
-driver.quit()
+# driver.quit()
